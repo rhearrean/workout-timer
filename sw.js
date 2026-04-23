3a7a6c47f19a87b6ab2d8219db845a46a2c4c618
rhearrean <56322115+rhearrean@users.noreply.github.com>
2026-04-22 20:18:23 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,23 @@
 const cacheName = 'workout-timer-v1';
+
 const filesToCache = ['./', './index.html'];
 
 self.addEventListener('install', e => {
+
   e.waitUntil(
+
     caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+
   );
+
 });
 
 self.addEventListener('fetch', e => {
+
   e.respondWith(
+
     caches.match(e.request).then(response => response || fetch(e.request))
+
   );
+
 });
